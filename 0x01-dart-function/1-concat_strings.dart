ddab1237f7386b3755a4a54cfe38a357e7562ac0
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-20 10:10:45 -0500

    push string

---
+++ 0x01-dart-function/1-concat_strings.dart
@@ -0,0 +1,8 @@
+String concatStr(String str1, String str2) {
+  return '$str1 $str2';
+}
+
+void main() {
+  String result = concatStr('Hello', 'World'); // Example usage
+  print(result); // Output: Hello World
+}
