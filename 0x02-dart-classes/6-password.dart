80d9210ac5e7ef9384acead83815d633f0c6bdfa
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-22 13:11:22 -0500

    pushhhh

---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,23 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    return _password.length >= 8 &&
+        _password.contains(RegExp(r'[A-Z]')) &&
+        _password.contains(RegExp(r'[a-z]')) &&
+        _password.contains(RegExp(r'[0-9]'));
+  }
+
+  String get password => _password;
+
+  set password(String password) {
+    _password = password;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
