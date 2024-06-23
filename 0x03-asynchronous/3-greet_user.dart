8dce856562826e040c197dd70d8b5e083b4d62d0
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-06-23 10:06:40 -0500

    Push question 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> userMap = json.decode(userData);
+    return 'Hello ${userMap['username']}';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool credentialsValid = await checkCredentials();
+    print('There is a user: $credentialsValid');
+    if (credentialsValid) {
+      return await greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+void main() async {
+  print(await loginUser());
+}
